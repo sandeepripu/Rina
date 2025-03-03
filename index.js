82b9612e2bdbd726abdd2a11c7fbf05ff9ccec86
sandeepripu <sandeepsahu7682@gmail.com>
2025-03-03 08:52:41 +0530

    Fixed index file path

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from "src/main/webapp"
+app.use(express.static(path.join(__dirname, "src/main/webapp")));
 
 app.get("/", (req, res) => {
-    res.send("Server is running!");
+    res.sendFile(path.join(__dirname, "src/main/webapp", "index.html"));
 });
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
